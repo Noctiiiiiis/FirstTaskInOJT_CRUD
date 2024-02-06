95550b5bf18c309fa5a2db138bce51be01763d27
ingle <inglepauldeleon37@gmail.com>
2024-02-06 16:45:41 +0800

    add a new file

---
+++ loremipsum.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HELLO WORLD</title>
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Delectus voluptas doloribus a consequuntur modi fuga earum placeat dolores ex commodi. Nam, dolor eveniet distinctio praesentium excepturi amet ad aspernatur laborum.
+</head>
+<body>
+    
+</body>
+</html>
